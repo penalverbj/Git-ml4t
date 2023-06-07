a97204a560df5fbab2581146e861abb6922187b4
jpb6 <jpb6@gatech.edu>
2023-06-06 21:56:11 -0400

    Project 3: cleaned insane learner

--- assess_learners/InsaneLearner.py
+++ assess_learners/InsaneLearner.py
@@ -1,22 +1,16 @@
 import numpy as np
 import BagLearner as bg
 import LinRegLearner as lrl
-
-
 class InsaneLearner(object):
-
     def __init__(self, verbose=False):
         self.learner_list = []
         for i in range(20):
             self.learner_list.append(bg.BagLearner(lrl.LinRegLearner, kwargs={}, bags=20))
-
     def author(self):
-        return 'jpb6'  # replace tb34 with your Georgia Tech username
-
+        return 'jpb6'
     def add_evidence(self, data_x, data_y):
         for learner in self.learner_list:
             learner.add_evidence(data_x, data_y)
-
     def query(self, points):
         temp = []
         for l in self.learner_list:
