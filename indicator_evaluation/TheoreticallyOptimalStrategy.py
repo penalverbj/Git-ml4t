437f19909bca3b8fd5d71a851b98839fa143f9de
jpb6 <jpb6@gatech.edu>
2023-06-23 17:49:16 -0400

    Project 6: Project structure and files set up 2

--- indicator_evaluation/TheoreticallyOptimalStrategy.py
+++ indicator_evaluation/TheoreticallyOptimalStrategy.py
@@ -1,2 +1,5 @@
 def author():
     return 'jpb6'
+
+def testPolicy(symbol=”AAPL”, sd=dt.datetime(2010, 1, 1), ed=dt.datetime(2011,12,31), sv = 100000):
+    pass
--- indicator_evaluation/testproject.py
+++ indicator_evaluation/testproject.py
@@ -1,2 +1,8 @@
 def author():
     return 'jpb6'
+
+def test_code():
+    pass
+
+if __name__ == "__main__":
+    test_code()
