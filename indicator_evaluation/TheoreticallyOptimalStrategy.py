062a3bf5c1530ff4985bd80aead883ce507d2dbf
jpb6 <jpb6@gatech.edu>
2023-06-26 19:39:07 -0400

    Project 6: Optimal policy and marketism done

--- indicator_evaluation/TheoreticallyOptimalStrategy.py
+++ indicator_evaluation/TheoreticallyOptimalStrategy.py
@@ -1,5 +1,34 @@
+import datetime as dt
+import pandas as pd
+import util
+
+
 def author():
     return 'jpb6'
 
-def testPolicy(symbol=”AAPL”, sd=dt.datetime(2010, 1, 1), ed=dt.datetime(2011,12,31), sv = 100000):
-    pass
+
+def testPolicy(symbol='AAPL', sd=dt.datetime(2010, 1, 1), ed=dt.datetime(2011, 12, 31), sv=100000):
+    prices = pd.DataFrame(util.get_data([symbol], pd.date_range(sd, ed)))
+    if (symbol != 'SPY'):
+        prices = prices.drop(['SPY'], axis=1)
+
+    prices['shift'] = prices.shift(periods=-1, axis="rows") - prices
+    prices['shift'] /= abs(prices['shift'])
+
+    prices.fillna(method='bfill', inplace=True)
+
+    trades = pd.DataFrame(data=0, index=prices.index, columns={symbol})
+    old_position = prices.iloc[0, -1]
+    trades[symbol] = old_position * 1000
+
+    for row, col in prices[1:].iterrows():
+        if col['shift'] != old_position:
+            trades.loc[row] = old_position * -2000
+            old_position = col['shift']
+        else:
+            trades.loc[row] = 0
+
+    trades.loc[-1] = 0
+    return trades
+
+
--- indicator_evaluation/testproject.py
+++ indicator_evaluation/testproject.py
@@ -1,8 +1,13 @@
+import TheoreticallyOptimalStrategy as tos
+from marketsimcode import compute_portvals
+
 def author():
     return 'jpb6'
 
 def test_code():
-    pass
+    optimalTrades = tos.testPolicy(symbol='JPM')
+    optimalPortVals = compute_portvals(orders_df=optimalTrades, impact=0, commission=0)
+    print(optimalPortVals)
 
 if __name__ == "__main__":
     test_code()
